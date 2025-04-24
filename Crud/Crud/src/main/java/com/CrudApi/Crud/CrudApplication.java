9fd82f6028aeaca6587a44efad7a6a10f8c8cb2e
Juliana Pelozo Pacheco <123774972+JulianaPelozo@users.noreply.github.com>
2025-04-24 16:33:30 -0300

    controller e dto

--- Crud/Crud/src/main/java/com/CrudApi/Crud/CrudApplication.java
+++ Crud/Crud/src/main/java/com/CrudApi/Crud/CrudApplication.java
@@ -1,4 +1,4 @@
-package com.CrudApi.Crud;
+package com.CrudApi.Crud; //commit
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
