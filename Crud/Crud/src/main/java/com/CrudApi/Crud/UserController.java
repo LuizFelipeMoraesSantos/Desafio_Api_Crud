130e4dc1788c669ec61dd8746e81c65d9ed6c5f8
Juliana Pelozo Pacheco <123774972+JulianaPelozo@users.noreply.github.com>
2025-05-03 14:19:42 -0300

    feat início da criação do Service e do Controller

--- Crud/Crud/src/main/java/com/CrudApi/Crud/UserController.java
+++ Crud/Crud/src/main/java/com/CrudApi/Crud/UserController.java
@@ -1,4 +1,15 @@
 package com.CrudApi.Crud;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping ("/list")
+    public String listUsers() {
+        return "users/list";
+    }
 }
--- Crud/Crud/src/main/java/com/CrudApi/Crud/UserService.java
+++ Crud/Crud/src/main/java/com/CrudApi/Crud/UserService.java
@@ -1,45 +1,13 @@
 package com.CrudApi.Crud;
 
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
-    private final UserRepository repository;
-    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    UserController userController = new UserController();
 
-    public UserService(UserRepository repository) {
-        this.repository = repository;
-    }
 
-    public UserResponseDTO cadastrar(UserRequestDTO dto) {
-        validarDados(dto);
-
-        User user = new User();
-        user.setNome(dto.getNome());
-        user.setEmail(dto.getEmail());
-        user.setSenha(passwordEncoder.encode(dto.getSenha()));
-        user.setDataNascimento(dto.getDataNascimento());
-        user.setTelefone(dto.getTelefone());
-        user.setEndereco(dto.getEndereco());
-        user.setCidade(dto.getCidade());
-        user.setEstado(dto.getEstado());
-        user.setAtivo(dto.getAtivo());
-
-        User salvo = repository.save(user);
-        return toResponseDTO(salvo);
-    }
-
-    public List<UserResponseDTO> listarTodos() {
-        return repository.findAll()
-                .stream()
-                .map(this::toResponseDTO)
-                .collect(Collectors.toList());
-    }
-
+}
